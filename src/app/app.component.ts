85c5aa530b7cf21031fb5b21193891f012911226
Shahen Aleksanyan <shahen.alexanyan@menugroup.co.uk>
2019-07-24 14:39:28 +0400

    testimonial

---
+++ src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  data = [{
+    imgUrl: 'assets/images/man.png',
+    userName: 'blabla',
+    text: 'I rely on my monthly rental income to help fund my retirement and this will help protect my nest egg'
+  }, {
+    imgUrl: 'assets/images/man.png',
+    userName: 'blabla',
+    text: 'I rely on my monthly rental income to help fund my retirement and this will help protect my nest egg'
+  }, {
+    imgUrl: 'assets/images/man.png',
+    userName: 'blabla',
+    text: 'I rely on my monthly rental income to help fund my retirement and this will help protect my nest egg'
+  }, {
+    imgUrl: 'assets/images/man.png',
+    userName: 'blabla',
+    text: 'I rely on my monthly rental income to help fund my retirement and this will help protect my nest egg'
+  }, {
+    imgUrl: 'assets/images/man.png',
+    userName: 'blabla',
+    text: 'I rely on my monthly rental income to help fund my retirement and this will help protect my nest egg'
+  }, {
+    imgUrl: 'assets/images/man.png',
+    userName: 'blabla',
+    text: 'I rely on my monthly rental income to help fund my retirement and this will help protect my nest egg'
+  }, {
+    imgUrl: 'assets/images/man.png',
+    userName: 'blabla',
+    text: 'I rely on my monthly rental income to help fund my retirement and this will help protect my nest egg'
+  }, {
+    imgUrl: 'assets/images/man.png',
+    userName: 'blabla',
+    text: 'I rely on my monthly rental income to help fund my retirement and this will help protect my nest egg'
+  }, {
+    imgUrl: 'assets/images/man.png',
+    userName: 'blabla',
+    text: 'I rely on my monthly rental income to help fund my retirement and this will help protect my nest egg'
+  }, {
+    imgUrl: 'assets/images/man.png',
+    userName: 'blabla',
+    text: 'I rely on my monthly rental income to help fund my retirement and this will help protect my nest egg'
+  }];
+}
